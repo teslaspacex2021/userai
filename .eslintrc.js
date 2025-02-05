07870b3303bdf81bcca0b707c470dc3ae3ca3316
teslaspacex2021 <teslaspacex2021@gmail.com>
2025-02-05 09:26:59 +0800

    0021

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = { extends: ["next/core-web-vitals"], rules: { "react/no-unescaped-entities": "off", "@typescript-eslint/no-unused-vars": "warn", "@typescript-eslint/no-empty-interface": "warn" } }
